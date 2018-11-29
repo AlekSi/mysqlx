16ff477773343baf95a48381edcc1efdde01c271
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2018-11-30 00:09:25 +0300

    Update copyright years.

--- datatypes.go
+++ datatypes.go
@@ -1,5 +1,5 @@
 // mysqlx - MySQL driver for Go's database/​sql package and MySQL X Protocol.
-// Copyright (c) 2017 Alexey Palazhchenko
+// Copyright (c) 2017-2018 Alexey Palazhchenko
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
